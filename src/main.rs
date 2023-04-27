2fdd7d519cdf913909452a71a4541cd26346ec55
Daniel Arany <adani32@gmail.com>
2023-04-27 21:42:03 +0200

    dual-core: Working dual core app but
    Second core is really slow or can't access resource soon enough
    Second core uses RAL instead of HAL

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use cortex_m_rt::entry;
 use stm32h7xx_hal::{pac, prelude::*};
+use stm32ral::{write_reg, rcc, gpio};
 use panic_halt as _;
 
 #[entry]
@@ -33,3 +34,19 @@ fn main() -> ! {
     }
 
 }
+
+unsafe extern "C" fn main2() -> ! {
+    write_reg!(rcc, RCC, AHB4ENR, GPIOBEN: Enabled);
+    write_reg!(gpio, GPIOB, MODER, MODER0: Output, MODER14: Output);
+    write_reg!(gpio, GPIOB, ODR, 0);
+    loop {
+        write_reg!(gpio, GPIOB, BSRR, BS0: 1);
+        cortex_m::asm::delay(16_000_000);
+        write_reg!(gpio, GPIOB, BSRR, BR0: 1);
+        cortex_m::asm::delay(16_000_000);
+    }
+}
+
+#[no_mangle]
+#[link_section=".flash2.reset_vector"]
+pub static CPU2_RESET_VECTOR: unsafe extern "C" fn() -> ! = main2;
