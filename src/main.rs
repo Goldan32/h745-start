b9a7bf9aa9f99208842b42524229a5396b08d4ea
Daniel Arany <adani32@gmail.com>
2023-11-12 10:52:23 +0000

    Move u8 arr formatter to new utils source file

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 #![no_std]
 #![allow(unused_imports)]
 
+mod utils;
+
 use hal::device::stk::cvr::CURRENT_R;
 use stm32h7xx_hal as hal;
 use panic_halt as _;
@@ -22,6 +24,7 @@ use smoltcp::socket::{TcpSocket, TcpSocketBuffer};
 use smoltcp::time::Instant;
 use smoltcp::wire::{EthernetAddress, IpAddress, IpCidr, IpEndpoint, Ipv4Address, Ipv6Cidr};
 use httparse;
+use crate::utils::write_to;
 
 // - logging ------------------------------------------------------------------
 
@@ -252,7 +255,7 @@ fn main() -> ! {
                                     
                                     // CRITICAL SECTION START
                                     log_serial!(tx, "Entered critical for CORE0\r\n");
-                                    let mut display_voltage = 0f32;
+                                    let display_voltage;
                                     unsafe {
                                         display_voltage = CURRENT_VOLTAGE;
                                     }
@@ -472,59 +475,5 @@ impl<'a> Net<'a> {
             Err(smoltcp::Error::Unrecognized) => (),
             Err(_) => (),
         };
-    }
-}
-
-// format! for u8 arrays
-
-pub mod write_to {
-    use core::cmp::min;
-    use core::fmt;
-
-    pub struct WriteTo<'a> {
-        buffer: &'a mut [u8],
-        // on write error (i.e. not enough space in buffer) this grows beyond
-        // `buffer.len()`.
-        used: usize,
-    }
-
-    impl<'a> WriteTo<'a> {
-        pub fn new(buffer: &'a mut [u8]) -> Self {
-            WriteTo { buffer, used: 0 }
-        }
-
-        pub fn as_str(self) -> Option<&'a str> {
-            if self.used <= self.buffer.len() {
-                // only successful concats of str - must be a valid str.
-                use core::str::from_utf8_unchecked;
-                Some(unsafe { from_utf8_unchecked(&self.buffer[..self.used]) })
-            } else {
-                None
-            }
-        }
-    }
-
-    impl<'a> fmt::Write for WriteTo<'a> {
-        fn write_str(&mut self, s: &str) -> fmt::Result {
-            if self.used > self.buffer.len() {
-                return Err(fmt::Error);
-            }
-            let remaining_buf = &mut self.buffer[self.used..];
-            let raw_s = s.as_bytes();
-            let write_num = min(raw_s.len(), remaining_buf.len());
-            remaining_buf[..write_num].copy_from_slice(&raw_s[..write_num]);
-            self.used += raw_s.len();
-            if write_num < raw_s.len() {
-                Err(fmt::Error)
-            } else {
-                Ok(())
-            }
-        }
-    }
-
-    pub fn show<'a>(buffer: &'a mut [u8], args: fmt::Arguments) -> Result<&'a str, fmt::Error> {
-        let mut w = WriteTo::new(buffer);
-        fmt::write(&mut w, args)?;
-        w.as_str().ok_or(fmt::Error)
     }
 }
